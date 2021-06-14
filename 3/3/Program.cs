02480cd94b06d198d7f804b62d8e81acfb765241
frenki19750 <kirill.frantskevich@gmail.com>
2021-06-14 23:10:34 +0300

    v1

---
+++ 3/3/Program.cs
@@ -0,0 +1,81 @@
+﻿using Org.BouncyCastle.Security;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "";
+            if (args.Length - 1 < 3)
+            {
+                Console.WriteLine("args must be >= 3");
+            }
+            else
+            {         
+
+                SecureRandom number = new SecureRandom();
+                int pcMove = 0;
+                pcMove = number.Next(1, args.Length);
+
+                string choisePK = args[pcMove];
+                byte[] hmac = hmacSHA256(choisePK, ref key);
+                Console.WriteLine("HMAC:" + BitConverter.ToString(hmac).Replace("-", "").ToLower());
+
+                Console.WriteLine("Available move:");
+                for (int i = 1; i < args.Length; i++)
+                {
+                    string argument = args[i];
+                    Console.Write("Move index ");
+                    Console.Write(i); // Write index
+                    Console.Write(" is [");
+                    Console.Write(argument); // Write string
+                    Console.WriteLine("]");
+                }
+                Console.Write("Enter your move: " );
+                int n = Convert.ToInt32(Console.ReadLine());
+                string ur = args[n];
+                Console.WriteLine("Your move: " + ur);
+                Console.WriteLine("Computer move: " + choisePK);
+
+                if(n == pcMove)
+                {
+                    Console.WriteLine("Draw");
+                }
+                int length = args.Length - 1 / 2;
+                if (pcMove < n)
+                {
+                    Console.WriteLine("You lose");
+                }
+                else if(pcMove > n)
+                {
+                    Console.WriteLine("You win");
+                }
+                Console.WriteLine("HMAC key:" + key);
+
+            }
+            static byte[] hmacSHA256(String data, ref string key)
+            {
+                key += RandomString(32);
+                using (HMACSHA256 hmac = new HMACSHA256(Encoding.ASCII.GetBytes(key)))
+                {
+                    return hmac.ComputeHash(Encoding.ASCII.GetBytes(data));
+                }
+            }
+            static string RandomString(int length)
+            {  
+                char[] allowableChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm1234567890".ToCharArray();
+                SecureRandom random = new SecureRandom();
+                string str = "";
+                for (var i = 0; i < length; i++)
+                {
+                    str += allowableChars[random.Next(0, 60)].ToString();
+                }
+                return new string(str);
+            }
+        }
+    }
+}
